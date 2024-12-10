047be929d04403e03eae7941449a615c59bb6d10
DarkHunterss <ap1617@student.aru.ac.uk>
2024-12-10 21:44:02 +0000

    updated the MembershipScreen so it that it fetches the data of memberships from the database and create the design dynamically

--- togetherCulture/MembershipScreen.Designer.cs
+++ togetherCulture/MembershipScreen.Designer.cs
@@ -17,139 +17,11 @@
 
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MembershipScreen));
-            this.panelHealth = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.communityMembershipBtn = new System.Windows.Forms.Button();
-            this.labelHealthDescription = new System.Windows.Forms.Label();
-            this.labelHealthTitle = new System.Windows.Forms.Label();
-            this.panelHealthPlus = new System.Windows.Forms.Panel();
-            this.label3 = new System.Windows.Forms.Label();
-            this.touchdownMembershipBtn = new System.Windows.Forms.Button();
-            this.labelHealthPlusDescription = new System.Windows.Forms.Label();
-            this.labelHealthPlusTitle = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.panel1 = new System.Windows.Forms.Panel();
             this.backMembershipBtn = new System.Windows.Forms.Button();
-            this.labelOtherTitle = new System.Windows.Forms.Label();
-            this.labelOtherDescription = new System.Windows.Forms.Label();
-            this.dedicatedMembershipBtn = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
-            this.panelOther = new System.Windows.Forms.Panel();
-            this.panelHealth.SuspendLayout();
-            this.panelHealthPlus.SuspendLayout();
-            this.tableLayoutPanel1.SuspendLayout();
-            this.panelOther.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // panelHealth
-            // 
-            this.panelHealth.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panelHealth.Controls.Add(this.label2);
-            this.panelHealth.Controls.Add(this.communityMembershipBtn);
-            this.panelHealth.Controls.Add(this.labelHealthDescription);
-            this.panelHealth.Controls.Add(this.labelHealthTitle);
-            this.panelHealth.Location = new System.Drawing.Point(3, 3);
-            this.panelHealth.Name = "panelHealth";
-            this.panelHealth.Size = new System.Drawing.Size(309, 388);
-            this.panelHealth.TabIndex = 1;
-            // 
-            // label2
-            // 
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(18, 61);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(274, 30);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "£18.50 / month";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // communityMembershipBtn
-            // 
-            this.communityMembershipBtn.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.communityMembershipBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.communityMembershipBtn.Location = new System.Drawing.Point(80, 333);
-            this.communityMembershipBtn.Name = "communityMembershipBtn";
-            this.communityMembershipBtn.Size = new System.Drawing.Size(140, 30);
-            this.communityMembershipBtn.TabIndex = 2;
-            this.communityMembershipBtn.Text = "Sign up now";
-            this.communityMembershipBtn.UseVisualStyleBackColor = true;
-            // 
-            // labelHealthDescription
-            // 
-            this.labelHealthDescription.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelHealthDescription.Location = new System.Drawing.Point(15, 100);
-            this.labelHealthDescription.Name = "labelHealthDescription";
-            this.labelHealthDescription.Size = new System.Drawing.Size(277, 192);
-            this.labelHealthDescription.TabIndex = 1;
-            this.labelHealthDescription.Text = resources.GetString("labelHealthDescription.Text");
-            this.labelHealthDescription.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            // 
-            // labelHealthTitle
-            // 
-            this.labelHealthTitle.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
-            this.labelHealthTitle.Location = new System.Drawing.Point(18, 10);
-            this.labelHealthTitle.Name = "labelHealthTitle";
-            this.labelHealthTitle.Size = new System.Drawing.Size(274, 30);
-            this.labelHealthTitle.TabIndex = 0;
-            this.labelHealthTitle.Text = "Community";
-            this.labelHealthTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // panelHealthPlus
-            // 
-            this.panelHealthPlus.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panelHealthPlus.Controls.Add(this.label3);
-            this.panelHealthPlus.Controls.Add(this.touchdownMembershipBtn);
-            this.panelHealthPlus.Controls.Add(this.labelHealthPlusDescription);
-            this.panelHealthPlus.Controls.Add(this.labelHealthPlusTitle);
-            this.panelHealthPlus.Location = new System.Drawing.Point(318, 3);
-            this.panelHealthPlus.Name = "panelHealthPlus";
-            this.panelHealthPlus.Size = new System.Drawing.Size(309, 388);
-            this.panelHealthPlus.TabIndex = 2;
-            // 
-            // label3
-            // 
-            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(17, 61);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(274, 30);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "£75 / month + £100 join fee";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // touchdownMembershipBtn
-            // 
-            this.touchdownMembershipBtn.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.touchdownMembershipBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.touchdownMembershipBtn.Location = new System.Drawing.Point(80, 333);
-            this.touchdownMembershipBtn.Name = "touchdownMembershipBtn";
-            this.touchdownMembershipBtn.Size = new System.Drawing.Size(140, 30);
-            this.touchdownMembershipBtn.TabIndex = 2;
-            this.touchdownMembershipBtn.Text = "Sign up now";
-            this.touchdownMembershipBtn.UseVisualStyleBackColor = true;
-            // 
-            // labelHealthPlusDescription
-            // 
-            this.labelHealthPlusDescription.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.labelHealthPlusDescription.Location = new System.Drawing.Point(14, 100);
-            this.labelHealthPlusDescription.Name = "labelHealthPlusDescription";
-            this.labelHealthPlusDescription.Size = new System.Drawing.Size(277, 192);
-            this.labelHealthPlusDescription.TabIndex = 1;
-            this.labelHealthPlusDescription.Text = "All of the benefits of Community Membership, plus\r\n\r\n24/7 Access to 5 Fitzroy Str" +
-    "eet\r\n\r\nInclusive hire for your own events and meetings\r\n\r\nAccess to the Workspac" +
-    "e workbench upstairs";
-            this.labelHealthPlusDescription.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            // 
-            // labelHealthPlusTitle
-            // 
-            this.labelHealthPlusTitle.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
-            this.labelHealthPlusTitle.Location = new System.Drawing.Point(18, 10);
-            this.labelHealthPlusTitle.Name = "labelHealthPlusTitle";
-            this.labelHealthPlusTitle.Size = new System.Drawing.Size(273, 30);
-            this.labelHealthPlusTitle.TabIndex = 0;
-            this.labelHealthPlusTitle.Text = "Workspace: Touchdown";
-            this.labelHealthPlusTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label1
             // 
@@ -159,22 +31,23 @@
             this.label1.Size = new System.Drawing.Size(948, 29);
             this.label1.TabIndex = 53;
             this.label1.Text = "Membership";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 3; // Default setup, dynamically updated
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.Controls.Add(this.panelOther, 2, 0);
-            this.tableLayoutPanel1.Controls.Add(this.panelHealthPlus, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.panelHealth, 0, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(25, 132);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(25, 100);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowCount = 1; // Default setup, dynamically updated
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(946, 394);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(946, 494);
             this.tableLayoutPanel1.TabIndex = 4;
+            this.tableLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.None; // Center dynamically
             // 
             // panel1
             // 
@@ -196,61 +69,6 @@
             this.backMembershipBtn.UseVisualStyleBackColor = true;
             this.backMembershipBtn.Click += new System.EventHandler(this.backMembershipBtn_Click);
             // 
-            // labelOtherTitle
-            // 
-            this.labelOtherTitle.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
-            this.labelOtherTitle.Location = new System.Drawing.Point(18, 10);
-            this.labelOtherTitle.Name = "labelOtherTitle";
-            this.labelOtherTitle.Size = new System.Drawing.Size(273, 30);
-            this.labelOtherTitle.TabIndex = 0;
-            this.labelOtherTitle.Text = "Workspace: Dedicated";
-            this.labelOtherTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // labelOtherDescription
-            // 
-            this.labelOtherDescription.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.labelOtherDescription.Location = new System.Drawing.Point(14, 100);
-            this.labelOtherDescription.Name = "labelOtherDescription";
-            this.labelOtherDescription.Size = new System.Drawing.Size(277, 192);
-            this.labelOtherDescription.TabIndex = 1;
-            this.labelOtherDescription.Text = "All of the benefits of Community Membership, plus\r\n\r\n24/7 Access to 5 Fitzroy Str" +
-    "eet\r\n\r\nInclusive hire for your own events and meetings\r\n\r\nYour own dedicated wor" +
-    "kspace and storage";
-            this.labelOtherDescription.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            // 
-            // dedicatedMembershipBtn
-            // 
-            this.dedicatedMembershipBtn.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.dedicatedMembershipBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dedicatedMembershipBtn.Location = new System.Drawing.Point(80, 333);
-            this.dedicatedMembershipBtn.Name = "dedicatedMembershipBtn";
-            this.dedicatedMembershipBtn.Size = new System.Drawing.Size(140, 30);
-            this.dedicatedMembershipBtn.TabIndex = 2;
-            this.dedicatedMembershipBtn.Text = "Sign up now";
-            this.dedicatedMembershipBtn.UseVisualStyleBackColor = true;
-            // 
-            // label4
-            // 
-            this.label4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(14, 61);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(274, 30);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "£100 / month + £100 join fee";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // panelOther
-            // 
-            this.panelOther.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panelOther.Controls.Add(this.label4);
-            this.panelOther.Controls.Add(this.dedicatedMembershipBtn);
-            this.panelOther.Controls.Add(this.labelOtherDescription);
-            this.panelOther.Controls.Add(this.labelOtherTitle);
-            this.panelOther.Location = new System.Drawing.Point(633, 3);
-            this.panelOther.Name = "panelOther";
-            this.panelOther.Size = new System.Drawing.Size(310, 388);
-            this.panelOther.TabIndex = 3;
-            // 
             // MembershipScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -261,35 +79,15 @@
             this.Controls.Add(this.label1);
             this.Name = "MembershipScreen";
             this.Size = new System.Drawing.Size(991, 649);
-            this.panelHealth.ResumeLayout(false);
-            this.panelHealthPlus.ResumeLayout(false);
-            this.tableLayoutPanel1.ResumeLayout(false);
-            this.panelOther.ResumeLayout(false);
             this.ResumeLayout(false);
-
+            this.PerformLayout();
         }
 
         #endregion
+
         private System.Windows.Forms.Label label1;
-
-        private System.Windows.Forms.Panel panelHealth;
-        private System.Windows.Forms.Button communityMembershipBtn;
-        private System.Windows.Forms.Label labelHealthDescription;
-        private System.Windows.Forms.Label labelHealthTitle;
-
-        private System.Windows.Forms.Panel panelHealthPlus;
-        private System.Windows.Forms.Button touchdownMembershipBtn;
-        private System.Windows.Forms.Label labelHealthPlusDescription;
-        private System.Windows.Forms.Label labelHealthPlusTitle;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button backMembershipBtn;
-        private System.Windows.Forms.Panel panelOther;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button dedicatedMembershipBtn;
-        private System.Windows.Forms.Label labelOtherDescription;
-        private System.Windows.Forms.Label labelOtherTitle;
     }
 }
--- togetherCulture/MembershipScreen.cs
+++ togetherCulture/MembershipScreen.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace togetherCulture
@@ -13,15 +9,186 @@ namespace togetherCulture
     public partial class MembershipScreen : UserControl
     {
         private MainWindow _mainWindow;
+
         public MembershipScreen(MainWindow mainWindow)
         {
             InitializeComponent();
             _mainWindow = mainWindow;
+
+            // Load memberships dynamically
+            LoadMemberships();
         }
 
         private void backMembershipBtn_Click(object sender, EventArgs e)
         {
             _mainWindow.backMembershipBtn_Click();
         }
+
+        private void LoadMemberships()
+        {
+            try
+            {
+                // Clear existing controls from the table layout
+                tableLayoutPanel1.Controls.Clear();
+
+                // Query to fetch the first three active memberships
+                string query = "SELECT TOP 3 Name, Benefits, MonthlyFee FROM membership WHERE IsActive = 1";
+
+                // Fetch data using DBConnection
+                DataTable membershipTable = DBConnection.getConnectionInstance().executeQuery(query);
+
+                int recordCount = membershipTable.Rows.Count;
+
+                if (recordCount == 0)
+                {
+                    // Display a message if no memberships are available
+                    Label noMembershipLabel = new Label
+                    {
+                        Text = "No membership available at the moment.",
+                        Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Bold),
+                        TextAlign = ContentAlignment.MiddleCenter,
+                        Dock = DockStyle.Fill
+                    };
+                    tableLayoutPanel1.Controls.Add(noMembershipLabel, 0, 0);
+                    tableLayoutPanel1.ColumnCount = 1;
+                    tableLayoutPanel1.RowCount = 1;
+                }
+                else
+                {
+                    int column = 0;
+
+                    foreach (DataRow row in membershipTable.Rows)
+                    {
+                        // Fetch data from the DataTable
+                        string name = row["Name"].ToString();
+                        string benefits = row["Benefits"].ToString();
+                        decimal monthlyFee = Convert.ToDecimal(row["MonthlyFee"]);
+
+                        // Create and add membership panel to the table layout
+                        var membershipPanel = CreateMembershipPanel(name, benefits, monthlyFee);
+                        tableLayoutPanel1.Controls.Add(membershipPanel, column % 3, column / 3);
+                        column++;
+                    }
+
+                    // Adjust layout if memberships are available
+                    AdjustTableLayout(recordCount);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading memberships: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void AdjustTableLayout(int recordCount)
+        {
+            int columns = Math.Min(recordCount, 3); // Max 3 columns
+            int rows = (int)Math.Ceiling((double)recordCount / columns);
+
+            tableLayoutPanel1.ColumnCount = columns;
+            tableLayoutPanel1.RowCount = rows;
+
+            tableLayoutPanel1.ColumnStyles.Clear();
+            tableLayoutPanel1.RowStyles.Clear();
+
+            for (int i = 0; i < columns; i++)
+                tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F / columns));
+
+            for (int i = 0; i < rows; i++)
+                tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.AutoSize));
+
+            // Center the table layout panel
+            tableLayoutPanel1.Anchor = AnchorStyles.None;
+            tableLayoutPanel1.Location = new Point(
+                (this.Width - tableLayoutPanel1.Width) / 2,
+                (this.Height - tableLayoutPanel1.Height) / 2
+            );
+        }
+
+        private Panel CreateMembershipPanel(string name, string benefits, decimal monthlyFee, string extraFee = "")
+        {
+            // Create a new panel
+            Panel panel = new Panel
+            {
+                BorderStyle = BorderStyle.FixedSingle,
+                Size = new Size(300, 420),
+                Margin = new Padding(15), // Adjusted for spacing between panels
+                BackColor = Color.White // Set white background for panels
+            };
+
+            // Membership name
+            Label nameLabel = new Label
+            {
+                Text = name,
+                Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold),
+                TextAlign = ContentAlignment.MiddleCenter,
+                Dock = DockStyle.Top,
+                Height = 40 // Height adjusted for consistent spacing
+            };
+
+            // Membership fee
+            Label feeLabel = new Label
+            {
+                Text = extraFee == string.Empty
+                    ? $"£{monthlyFee:0.00} / month"
+                    : $"£{monthlyFee:0.00} / month + {extraFee}",
+                Font = new Font("Segoe UI", 10F, FontStyle.Bold),
+                TextAlign = ContentAlignment.MiddleCenter,
+                Dock = DockStyle.Top,
+                Height = 30 // Adjusted for better spacing
+            };
+
+            // Membership benefits
+            Label benefitsLabel = new Label
+            {
+                Text = FormatBenefits(benefits), // Each benefit on a new line
+                Font = new Font("Microsoft Sans Serif", 10F), // Larger font size for readability
+                TextAlign = ContentAlignment.TopCenter, // Centered text
+                Dock = DockStyle.Fill,
+                Padding = new Padding(10, 5, 10, 5), // Padding for spacing
+            };
+
+            // Sign up button
+            Button signUpButton = new Button
+            {
+                Text = "Sign up now",
+                Font = new Font("Segoe UI", 10F, FontStyle.Regular),
+                Dock = DockStyle.Bottom,
+                Height = 40,
+                Cursor = Cursors.Hand,
+                BackColor = Color.LightGray, // Button background color
+                FlatStyle = FlatStyle.Flat,
+                ForeColor = Color.Black // Consistent text color
+            };
+
+            // Explicitly remove all border styling
+            signUpButton.FlatAppearance.BorderSize = 0;
+            signUpButton.FlatAppearance.MouseDownBackColor = Color.LightGray; // Match background
+            signUpButton.FlatAppearance.MouseOverBackColor = Color.DarkGray; // Slight hover effect
+
+            signUpButton.Click += (sender, e) =>
+            {
+                MessageBox.Show($"You have selected the {name} membership.", "Membership Selected", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            };
+
+            // Add controls to the panel
+            panel.Controls.Add(signUpButton);
+            panel.Controls.Add(benefitsLabel);
+            panel.Controls.Add(feeLabel);
+            panel.Controls.Add(nameLabel);
+
+            return panel;
+        }
+
+
+        private string FormatBenefits(string benefits)
+        {
+            // Split benefits by comma and add a new line for each benefit
+            string[] benefitList = benefits.Split(',');
+            return string.Join(Environment.NewLine + Environment.NewLine, benefitList.Select(b => b.Trim()));
+        }
+
+
     }
 }
